5c7a1f05f0a13682dbf64e38d7adc84e45a97f4e
momo1627 <momo1627@gmail.com>
2019-01-07 14:24:56 +1100

    01react finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import * as serviceWorker from './serviceWorker';
+// import './index.css';
 // import App from './App';
+
+//assignment 1
+import './assignment/01react.css'
+import img from './assignment/corgi.jpg'
 import Assign1 from './assignment/01react';
-import * as serviceWorker from './serviceWorker';
-const img = 'assignment/corgi.jpg'
 const title = 'dog'
 const subtitle ='corgi'
-const descrpition = 'cute'
-const element = <Assign1 title={title} img={img} subtitle={subtitle} descrpition={descrpition} /> 
+const description = 'cute'
+const element = <Assign1 title={title} img={img} subtitle={subtitle} description={description} /> 
 ReactDOM.render(element, document.getElementById('root'));
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
